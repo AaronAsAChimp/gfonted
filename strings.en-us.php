a38dabc12752ba57d584bf88edd44455c4b1eb10
Aaron Spaulding <Aaron@sachimp.com>
2009-06-30 14:12:18 -0400

    Moved some stuff around

---
+++ strings.en-us.php
@@ -0,0 +1,7 @@
+<?php
+	define("GFE_STRINGS_UTF_CODEPOINT", "UTF Codepoint");
+	define("GFE_STRINGS_NUMBER_SEGMENTS", "Number of Segments");
+	define("GFE_STRINGS_MAXIMUM_CHILDREN", "Maximum Number of Children");
+	
+	define("GFE_STRINGS_CHECK_FITNESS_CTA", "");
+?>
